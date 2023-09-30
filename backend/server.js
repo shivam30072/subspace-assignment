3020293ad9813239e452d0d66074fd7be961316a
shivam30072 <shivam30072@gmail.com>
2023-10-01 01:43:29 +0530

    env added

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,11 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
 
 const blogRouter = require("./routes/blogRoute");
+const { config } = require("dotenv");
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.use("/api", blogRouter);
 
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
